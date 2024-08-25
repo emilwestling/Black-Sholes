036c29fab63150319a3ee3f469ad6d391c2e71ee
emil-westling <westling01@gmail.com>
2024-08-25 00:00:06 -0400

    app.run() changed

--- src/app.py
+++ src/app.py
@@ -32,4 +32,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
